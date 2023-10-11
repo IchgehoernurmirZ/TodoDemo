d244b68af51bfe8d663ce1f9545538c43e6b0918
IchgehoernurmirZ <Zoey42zhang@gmail.com>
2023-10-10 21:16:17 -0400

    create AddTodo button and its function

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -8,6 +8,17 @@ function App() {
     {rowNumber: 2, rowDescription: 'Water Plant', rowAssigned: 'User Two'},
     {rowNumber: 3, rowDescription: 'Make Dinner', rowAssigned: 'User Three'},
   ]
+
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {rowNumber: todos.length + 1,
+      rowDescription: 'new todo',
+      rowAssigned: 'User Three'
+      }
+      todos.push(newTodo);
+    }
+  }
+
   return (
     <div className="mt-5 container">
      <div className="card">
@@ -16,6 +27,9 @@ function App() {
       </div>
       <div className="card-body">
           <TodoTable todos={todos}/>
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add New Todo
+          </button>
       </div>
      </div>
     </div>
